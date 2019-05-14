271604988392c84ccedfdff56525730f63b1dd52
pmhofmann <pm.hofmann@yahoo.co.uk>
2019-05-14 15:00:22 +0100

    Patrick and Ash deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const formEl = document.querySelector('#create-task-form')
+const tasks = document.querySelector('#tasks')
+
+
+function addTask(task){
+  const li = document.createElement("li")
+
+  li.innerHTML = task 
+
+  tasks.appendChild(li)
+
+
+}
+
+
+
+
+
+
+
+formEl.addEventListener('submit', function(e){
+  e.preventDefault()
+  const task = formEl["new-task-description"].value
+
+
+  if (task.length > 3){
+    addTask(task)
+  }  else {
+    alert("nahh mate, type again.")
+  }
+  
+
+
+  formEl.reset()
+
+
+
+})
